cfc283defef9b32a5029c1646c2ae45145a6b5d6
Andrii Nemeritskyy <andrey.nemeritskyy@gmail.com>
2025-01-18 15:23:36 +0200

    Add prettier, init empty project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,25 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: 'airbnb-base',
-    overrides: [
-      {
-        env: {
-          node: true,
-        },
-        files: [
-          '.eslintrc.{js,cjs}',
-        ],
-        parserOptions: {
-          sourceType: 'script',
-        },
-      },
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {
-      'linebreak-style': 'off',
-    },
-  };
-  
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'airbnb-base',
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'linebreak-style': 'off',
+	},
+};
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	arrowParens: 'always',
+	htmlWhitespaceSensitivity: 'css',
+};
